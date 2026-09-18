01a96f833fe8746ecd9f4bb4619f4a077fc3c422
jessemj117-lgtm <jessemj117@gmail.com>
2026-09-18 23:15:55 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,8 +2,7 @@ const SUPABASE_URL = "https://axvtbdystaorknqmshgb.supabase.co";
 
 const SUPABASE_ANON_KEY =
     "sb_publishable_foSYwlGFQhVlenpa_dBF8g_7092TFz5";
-
-const supabaseClient = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
