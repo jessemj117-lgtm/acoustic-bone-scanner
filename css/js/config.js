b9db25ae0b033502f797fd4b358e665419e12289
jessemj117-lgtm <jessemj117@gmail.com>
2026-09-18 23:00:03 +0530

    Create config.js

---
+++ css/js/config.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://axvtbdystaorknqmshgb.supabase.co";
+
+const SUPABASE_ANON_KEY =
+    "sb_publishable_foSYwlGFQhVlenpa_dBF8g_7092TFz5";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
